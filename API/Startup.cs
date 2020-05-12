444fe696cebc47acbb3cdb1c5d54cea38634bba6
Saban <SABANNX@legal.regn.net>
2020-05-12 21:56:25 +0200

    Generics and started implementing automapper

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,5 @@
+using API.Helpers;
+using AutoMapper;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
@@ -22,6 +24,9 @@ namespace API
             services.AddDbContext<StoreContext> (x =>
                 x.UseSqlite (_config.GetConnectionString ("DefaultConnection")));
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+
+            services.AddAutoMapper(typeof(MappingProfiles));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
