87182a30b82d3c97e3b4ff601535d65d5eda4b72
Saban <SABANNX@legal.regn.net>
2020-06-07 21:35:35 +0200

    Added angular project

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,9 +27,16 @@ namespace API
             services.AddDbContext<StoreContext>(x =>
                x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddAutoMapper(typeof(MappingProfiles));
-            
+
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("htpps://localhost:5001");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,8 +50,10 @@ namespace API
             app.UseRouting();
             app.UseStaticFiles();
 
+            app.UseCors("CorsPolicy");
+
             app.UseAuthorization();
-            
+
             app.UseSwaggerDocumentation();
             app.UseEndpoints(endpoints =>
             {
