b4026521234cc625075cf05e6153e9c9c733eb3a
Saban <SABANNX@legal.regn.net>
2020-06-30 22:00:06 +0200

    Error handling on client side

---
+++ client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.scss']
+})
+export class TestErrorComponent implements OnInit {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'product/42').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'Buggy/servererror').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'Buggy/badrequest').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  get400ValidationError() {
+    this.http.get(this.baseUrl + 'products/fortytwo').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+}
